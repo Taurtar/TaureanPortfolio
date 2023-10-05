62ac1a1c08ed7b66c7d4110de168c2dcc8832c08
Taurtar <115844981+Taurtar@users.noreply.github.com>
2023-10-04 20:49:32 -0500

    Update Program2.4.java

--- Program2.4.java
+++ Program2.4.java
@@ -1,15 +1,16 @@
+import java.util.*;
 class Main{
     public static void main(String[] args){
-        Book b1 = new Book();
+        Book book = new Book();
 
     }
-    class Book{
-        String title = null;
-        ArrayList auther = new ArrayList();
-        String publisher = null;
-        float price = 0;
-        int copies = 0;
+}
+class Book{
+    String title = null;
+    ArrayList auther = new ArrayList();
+    String publisher = null;
+    float price = 0;
+    int copies = 0;
 
 
-    }
 }
