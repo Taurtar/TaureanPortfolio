75b41d20e998087a59a099d59f29251d783bad79
Taurtar <115844981+Taurtar@users.noreply.github.com>
2023-10-09 17:10:49 -0500

    Add files via upload

---
+++ Program2.1.java
@@ -0,0 +1,16 @@
+/*import java.util.*;
+class Main{
+    public static void main(String[] args){
+        //Write a program that can convert any decimal integers to binary format.
+        Scanner console = new Scanner(System.in);
+        String conversion = null;
+        System.out.println("Enter a decimal integer: ");
+        int decimal = console.nextInt();
+        
+        conversion  = Integer.toBinaryString(decimal);
+        System.out.print(conversion);
+       
+        console.close();
+    }
+}
+*/
---
+++ Program2.2.java
@@ -0,0 +1,23 @@
+/*import java.util.*; 
+class Main {
+    public static void main(String[] args){
+        Random random = new Random();
+        Scanner console = new Scanner(System.in);
+        int randomNum = random.nextInt(10,90);
+        int guess = 0;
+        int tries = 0;
+
+
+        System.out.println(" Guess a number between 10 and 90: ");
+        do{
+            guess = console.nextInt();
+            tries++;
+            if(tries >= 1){
+                if(guess > randomNum) System.out.println("The number is lower!");
+                else if(guess < randomNum) System.out.println("The number is higher!");
+            }
+        }while(guess != randomNum);
+        System.out.println(" YOU WIN!!! It took you " + tries + " tries");
+    }
+}
+*/
